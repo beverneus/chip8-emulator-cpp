cac85875846736573f1cdba411c178280567a01d
Lars De Volder <36573562+beverneus@users.noreply.github.com>
2025-03-29 00:23:29 +0100

    restructure

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,26 @@
+// Copyright 2025 Lars De Volder
+#include <math.h>
+#include <SDL2/SDL.h>
 #include <cstdint>
 #include <iostream>
 #include <stack>
-#include <math.h>
-#include <SDL2/SDL.h>
 
-#define GET_X(opcode)   ((opcode & 0x0F00) >> 8)
-#define GET_Y(opcode)   ((opcode & 0x00F0) >> 4)
-#define GET_N(opcode)   ( opcode & 0x000F)
-#define GET_NN(opcode)  ( opcode & 0x00FF)
-#define GET_NNN(opcode) ( opcode & 0x0FFF)
+#define GET_X(opcode) ((opcode & 0x0F00) >> 8)
+#define GET_Y(opcode) ((opcode & 0x00F0) >> 4)
+#define GET_N(opcode) (opcode & 0x000F)
+#define GET_NN(opcode) (opcode & 0x00FF)
+#define GET_NNN(opcode) (opcode & 0x0FFF)
+
+#define SCREEN_WIDTH 64
+#define SCREEN_HEIGHT 32
+#define SCALE_FACTOR 8
+#define WINDOW_WIDTH (SCREEN_WIDTH * SCALE_FACTOR)
+#define WINDOW_HEIGHT (SCREEN_HEIGHT * SCALE_FACTOR)
+
+SDL_Window *window = nullptr;
+SDL_Surface *winSurface = nullptr;
+SDL_Surface *chip8Surface = nullptr;
+uint8_t display[SCREEN_WIDTH * SCREEN_HEIGHT] = {};
 
 struct Registers {
     uint PC = 0x200;
@@ -37,39 +49,36 @@ class Chip8 {
 
     uint16_t opcode = 0x0;
 
-    public:
-        void fetch() {
-            uint* PC = &this->regs.PC;
-            const uint8_t a = this->memory.read(*PC);
-            const uint8_t b = this->memory.read(*PC + 1);
-            *PC += 2;
-            opcode = a << 8 | b;
-        }
+        public:
+            void fetch() {
+                uint* PC = &this->regs.PC;
+                const uint8_t a = this->memory.read(*PC);
+                const uint8_t b = this->memory.read(*PC + 1);
+                *PC += 2;
+                opcode = a << 8 | b;
+            }
 
-        void decode() {
-
-        }
-
+            void decode() {
+            }
 };
 
-#define SCREEN_WIDTH 64
-#define SCREEN_HEIGHT 32
-#define SCALE_FACTOR 4
-#define WINDOW_WIDTH (SCREEN_WIDTH * SCALE_FACTOR)
-#define WINDOW_HEIGHT (SCREEN_HEIGHT * SCALE_FACTOR)
-
-SDL_Window* window = nullptr;
-SDL_Surface* winSurface = nullptr;
-SDL_Surface* chip8Surface = nullptr;
-uint8_t display[SCREEN_WIDTH * SCREEN_HEIGHT] = {};
+void draw() {
+    SDL_FillRect(chip8Surface, nullptr,
+        SDL_MapRGB(chip8Surface->format, 0, 0, 0));
+}
 
 bool loop();
 
 int main() {
     SDL_Init(SDL_INIT_EVERYTHING);
 
-    window = SDL_CreateWindow("Chip-8 emulator", 100, 100, 64, 32, SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow("Chip-8",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
     winSurface = SDL_GetWindowSurface(window);
+
+    chip8Surface = SDL_CreateRGBSurface(0,
+        SCREEN_WIDTH, SCREEN_HEIGHT, 32, 0, 0, 0, 0);
 
     while ( loop() ) {
     }
@@ -82,7 +91,7 @@ bool loop() {
     const Uint64 start = SDL_GetPerformanceCounter();
     // Event loop
     SDL_Event evt;
-    while (SDL_PollEvent( &evt ) != 0) {
+    while (SDL_PollEvent(&evt) != 0) {
         switch (evt.type) {
             case SDL_QUIT:
                 return false;
@@ -92,7 +101,8 @@ bool loop() {
     }
 
     const Uint64 end = SDL_GetPerformanceCounter();
-    const float elapsedMS = (end - start) / static_cast<float>(SDL_GetPerformanceFrequency()) * 1000.0f;
+    const float elapsedMS = (end - start) /
+        static_cast<float>(SDL_GetPerformanceFrequency()) * 1000.0f;
 
     SDL_Delay(fmax(0.0f, floor(16.666f - elapsedMS)));
 
@@ -100,4 +110,3 @@ bool loop() {
 
     return true;
 }
-
