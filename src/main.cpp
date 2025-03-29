cc2e8fa37cff37aec6c735db07bd85d7431a5a7a
Lars De Volder <36573562+beverneus@users.noreply.github.com>
2025-03-29 15:32:06 +0100

    IBM logo works

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,11 @@
 #include <math.h>
 #include <SDL2/SDL.h>
 #include <cstdint>
+#include <cstring>
 #include <iostream>
 #include <stack>
+#include <iostream>
+#include <fstream>
 
 #define GET_X(opcode) ((opcode & 0x0F00) >> 8)
 #define GET_Y(opcode) ((opcode & 0x00F0) >> 4)
@@ -43,7 +46,7 @@ void draw()
 }
 
 struct Registers {
-    uint PC = 0x200;
+    uint16_t PC = 0x200;
     uint16_t I = 0;
     uint8_t V[16] = {};
     uint8_t delayTimer = 0;
@@ -63,37 +66,102 @@ struct Memory {
 };
 
 class Chip8 {
-    Registers regs;
-    Memory memory;
-    std::stack<int> stack;
-
-    uint16_t opcode = 0x0;
 
         public:
+            Registers regs;
+            Memory memory;
+            std::stack<int> stack;
+
+            uint16_t opcode = 0x0;
+
             void fetch() {
-                uint* PC = &this->regs.PC;
-                const uint8_t a = this->memory.read(*PC);
-                const uint8_t b = this->memory.read(*PC + 1);
-                *PC += 2;
+                const uint8_t a = this->memory.read(this->regs.PC);
+                const uint8_t b = this->memory.read(this->regs.PC + 1);
+                this->regs.PC += 2;
                 opcode = a << 8 | b;
             }
 
             void decode() {
+                uint8_t category = (opcode & 0xF000) >> 12;
+                switch (category) {
+                    case 0x0:
+                        switch (GET_NN(opcode)) {
+                            case 0xE0:
+                                std::memset(display, 0, sizeof(display));
+                                break;
+                        }
+                        break;
+                    case 0x1:
+                        this->regs.PC = GET_NNN(opcode);
+                        break;
+                    case 0x6:
+                        this->regs.V[GET_X(opcode)] = GET_NN(opcode);
+                        break;
+                    case 0x7:
+                        this->regs.V[GET_X(opcode)] += GET_NN(opcode);
+                        break;
+                    case 0xA:
+                        this->regs.I = GET_NNN(opcode);
+                        break;
+                    case 0xD:
+                        uint8_t sprite_x = this->regs.V[GET_X(opcode)] % SCREEN_WIDTH;
+                        uint8_t sprite_y = this->regs.V[GET_Y(opcode)] % SCREEN_HEIGHT;
+                        uint8_t* sprite = &this->memory.data[this->regs.I];
+                        uint8_t* VF = &this->regs.V[0xF];
+                        *VF = 0;
+                        for (int y = 0; y < GET_N(opcode); y++) {
+                            for (int x = 0; x < 8; x++) {
+                                if (*(sprite + y) & (0b1 << 7-x)) {
+                                    int draw_x = sprite_x + x;
+                                    int draw_y = sprite_y + y;
+                                    if (draw_x < SCREEN_WIDTH && draw_y < SCREEN_HEIGHT) {
+                                        if (display[draw_y * SCREEN_WIDTH + draw_x]) {
+                                            *VF = 1;
+                                            display[draw_y * SCREEN_WIDTH + draw_x] = 0;
+                                        } else {
+                                            display[draw_y * SCREEN_WIDTH + draw_x] = 1;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        break;
+                }
             }
 };
 
 bool loop();
+Chip8 chip;
 
 int main() {
+
+    //ROM
+    std::ifstream rom("/home/larsdevolder/code/cpp/chip8/assets/roms/tests/IBM_Logo.ch8", std::ios::binary);
+    if (rom.fail()) {
+        std::cout << "Failed to read rom" << std::endl;
+    }
+    {
+        char x;
+        uint8_t i = 0;
+        while (!rom.eof()) {
+            rom.read(&x, 1);
+            chip.memory.data[0x200+i] = x;
+            i++;
+    }
+    }
+    rom.close();
+
+
+    //DISPLAY
     SDL_Init(SDL_INIT_EVERYTHING);
     window = SDL_CreateWindow("Chip-8", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
     winSurface = SDL_GetWindowSurface(window);
     chip8Surface = SDL_CreateRGBSurface(0, SCREEN_WIDTH, SCREEN_HEIGHT, 32, 0, 0, 0, 0);
 
+
     while ( loop() ) {
     }
 
-    Chip8 chip;
     return 0;
 }
 
@@ -108,6 +176,10 @@ bool loop() {
             default:
                 break;
         }
+    }
+    for (int i=0; i<12; i++) {
+        chip.fetch();
+        chip.decode();
     }
 
     draw();
