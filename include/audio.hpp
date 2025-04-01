3b7d66a1cb4b668a50f2685c6773f9fcbb314cc9
Lars De Volder <36573562+beverneus@users.noreply.github.com>
2025-04-02 00:56:40 +0200

    Remove unused variables from Beep

--- include/audio.hpp
+++ include/audio.hpp
@@ -1,14 +1,11 @@
 #ifndef AUDIO_HPP
 #define AUDIO_HPP
 
-#include <math.h>
 #include <SDL3/SDL.h>
 
 class Beep {
     SDL_AudioStream *stream;
     SDL_AudioSpec spec;
-    char *wav_path;
-    uint64_t current_sin_sample;
     public:
         Beep();
         void play();
