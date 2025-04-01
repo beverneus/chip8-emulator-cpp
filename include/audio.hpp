52f12f0df544613d789cec0da6546c625f000445
Lars De Volder <36573562+beverneus@users.noreply.github.com>
2025-04-01 20:27:19 +0200

    audio with sin function instead of wav

--- include/audio.hpp
+++ include/audio.hpp
@@ -1,15 +1,14 @@
 #ifndef AUDIO_HPP
 #define AUDIO_HPP
 
+#include <math.h>
 #include <SDL3/SDL.h>
 
 class Beep {
     SDL_AudioStream *stream;
-    uint8_t *wav_data;
-    uint32_t wav_data_len;
     SDL_AudioSpec spec;
     char *wav_path;
-
+    uint64_t current_sin_sample;
     public:
         Beep();
         void play();
--- src/audio.cpp
+++ src/audio.cpp
@@ -1,17 +1,16 @@
 #include "audio.hpp"
 
-Beep::Beep() :
- stream(nullptr), wav_data(nullptr), wav_data_len(0), wav_path(nullptr) {
+Beep::Beep() : 
+    stream(nullptr),
+    current_sin_sample(0)
+    {
     if(!SDL_Init(SDL_INIT_AUDIO)) {
         SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
     }
 
-    //Load WAV
-    SDL_asprintf(&wav_path, "%s../assets/audio/beep.wav", SDL_GetBasePath());
-    if (!SDL_LoadWAV(wav_path, &spec, &wav_data, &wav_data_len)) {
-        SDL_Log("Couldn't load .wav file: %s", SDL_GetError());
-    }
-    SDL_free(wav_path);
+    spec.channels = 1;
+    spec.format = SDL_AUDIO_F32;
+    spec.freq = 48000;
 
     stream = SDL_OpenAudioDeviceStream(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, &spec, NULL, NULL);
     if (!stream) (SDL_Log("Couldn't create audio stream: %s", SDL_GetError()));
@@ -20,6 +19,15 @@ Beep::Beep() :
 }
 
 void Beep::play() {
-    SDL_ClearAudioStream(stream);
-    SDL_PutAudioStreamData(stream, wav_data, wav_data_len);
+    static const int freq = 800;
+    float samples[static_cast<int>(static_cast<float>(spec.freq) / 60)];
+    for (size_t i = 0; i < SDL_arraysize(samples); i++) {
+        double phase = static_cast<double>(current_sin_sample) * freq / spec.freq;
+        samples[i] = 0.5f * SDL_sinf(phase * 2 * SDL_PI_F);
+        current_sin_sample++;
+    }
+
+    current_sin_sample %= spec.freq;
+
+    SDL_PutAudioStreamData(stream, samples, sizeof(samples));
 }
